6a2c78296cefccc9129197970691456e682df095
Monosaicus <39736542+AURBalcan@users.noreply.github.com>
2022-05-10 16:28:18 +0200

    Alineació butons i afegir botons MOD, potència i igual

--- Calculadora/Calculadora/Form1.cs
+++ Calculadora/Calculadora/Form1.cs
@@ -151,6 +151,16 @@ namespace Calculadora
 
             }
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
 
